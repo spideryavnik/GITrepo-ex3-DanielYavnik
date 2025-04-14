77d8e0c198806cd4187eff8f590227fd7cfc915a
Daniel Yavnik <daniel.yavnik@gmail.com>
2025-04-14 19:10:10 +0300

    prints my name multiple times no capital letters

--- DanielYavnik.java
+++ DanielYavnik.java
@@ -1,6 +1,6 @@
 public class DanielYavnik {
     public static void main(String[] args) {
-        String name = "different print";
+        String name = "daniel yavnik";
 
         for (int i = 0; i < 5; i++) {  // Change 5 to any number of times you want
             System.out.println(name);
