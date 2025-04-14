d448704f545c3d1b0894d270e47878838425ec15
Daniel Yavnik <daniel.yavnik@gmail.com>
2025-04-14 19:05:03 +0300

    changed the name printing to a different print

--- DanielYavnik.java
+++ DanielYavnik.java
@@ -1,6 +1,6 @@
 public class DanielYavnik {
     public static void main(String[] args) {
-        String name = "Daniel Yavnik";
+        String name = "different print";
 
         for (int i = 0; i < 5; i++) {  // Change 5 to any number of times you want
             System.out.println(name);
