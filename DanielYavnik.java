1953f500c21ca3639394810d762872ec823259b9
Daniel Yavnik <daniel.yavnik@gmail.com>
2025-04-14 19:01:59 +0300

    initial commit

---
+++ DanielYavnik.java
@@ -0,0 +1,9 @@
+public class DanielYavnik {
+    public static void main(String[] args) {
+        String name = "Daniel Yavnik";
+
+        for (int i = 0; i < 5; i++) {  // Change 5 to any number of times you want
+            System.out.println(name);
+        }
+    }
+}
