24907e1230bfe82c0c5a491623a274e6c1c1c115
Daniel Yavnik <daniel.yavnik@gmail.com>
2025-04-14 19:12:12 +0300

    adding ! to both prints

--- DanielYavnik.java
+++ DanielYavnik.java
@@ -1,6 +1,6 @@
 public class DanielYavnik {
     public static void main(String[] args) {
-        String name = "daniel yavnik";
+        String name = "daniel yavnik!";
 
         for (int i = 0; i < 5; i++) {  // Change 5 to any number of times you want
             System.out.println(name);
--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public static void main(String [] args)
 {
 for(int i=0;i<=10;i++)
 {
-System.out.println("whats up"+i);
+System.out.println("whats up!"+i);
 }
 }
 }
